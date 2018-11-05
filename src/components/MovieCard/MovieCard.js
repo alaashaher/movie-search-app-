a01eb7faa0dffb75f56b5d30e5bf21b6262cc8eb
unknown <alaashaher12@gmail.com>
2018-11-05 18:27:46 +0200

    desgin

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './MovieCard.css';
+
+const MovieCard = (props) => {
+    console.log(props);
+    return (
+        <div className="container">
+            <div className="movie-card">
+                <div className="movie-header" >
+                </div>
+                <div className="movie-content">
+                    <div className="movie-content-header" style={{ backgroundImage: `url(${props.movie.Poster})` }}>
+                        <h3 className="movie-title">{props.movie.Title}</h3>
+                    </div>
+                    <div className="movie-info">
+                        <div className="info-section">
+                            <label>Released</label>
+                            <span>{props.movie.Released}</span>
+                        </div>
+                        <div className="info-section">
+                            <label>IMDB Rating</label>
+                            <span>{props.movie.imdbRating}</span>
+                        </div>
+                        <div className="info-section">
+                            <label>Rated</label>
+                            <span>{props.movie.Rated}</span>
+                        </div>
+                        <div className="info-section">
+                            <label>Runtime</label>
+                            <span>{props.movie.Runtime}</span>
+                        </div>
+                    </div>
+                    <div className="plot" style={{fontSize: '12px'}}>
+                        <p>{props.movie.Plot}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
